5d7a315ec55ffe5af101e4b5756b5a4aa865b045
sagivsaraf <151636188+sagivsaraf@users.noreply.github.com>
2023-12-24 09:55:28 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+
+class Main {
+public static void main(String[] args) {
+ int x = 7;
+ int y = 9;
+ int z = 5;
+ System.out.println(x);
+ System.out.println(y);
+ System.out.println(z);
+ float w = 0.5f;
+ float e = 0.7f;
+ float r = 0.9f;
+ System.out.println(w);
+ System.out.println(e);
+ System.out.println(r);
+
+char str1 = 's';
+char str2 = 'a';
+char str3 = 'g';
+char str4 = 'i';
+char str5 = 'v';
+System.out.print(str1);
+System.out.print(str2);
+System.out.print(str3);
+System.out.print(str4);
+System.out.println(str5);
+  int num = 100;
+  int num2 =99;
+  System.out.println("num is bigger than num2");
+  int num3 = 5;
+  int num4 = 10;
+  int num5 = 15;
+  float score =(num3+num4+num5)/3;
+  System.out.println(score);
+  int height = 15;
+  int width = 10;
+  System.out.println(height*width);
+}
+}
